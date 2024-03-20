d39c6f8bbfefe74b31bc5ff0272badc468780c88
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-20 19:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const text=document.getElementById("text");
+const delay=document.getElementById("delay");
+
+const output=document.getElementById("output");
+const btn=document.getElementById("btn"); 
+
+function handleDelay() {
+	setTimeout(()=>{
+		output.innerText=text.value 
+	},delay.value)
+}
+
